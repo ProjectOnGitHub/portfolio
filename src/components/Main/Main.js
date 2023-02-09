ba4698747ce10053ec1a19e84e65fecef693c271
Aleksandr <soft@vhlv.ru>
2023-02-09 20:26:00 +0300

    feat: add Main style

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './_Main.scss';
-
+import Profile from '../Profile/Profile';
+import Section from '../Section/Section';
 function Main() {
-
-
   return (
-    <main className="main">
-      Main
-    </main >
+    <main className='main'>
+      <Section
+        name='profile'
+        id="about">
+        <Profile />
+      </Section>
+    </main>
   );
 }
 
