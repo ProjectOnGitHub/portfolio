a954860a72ea35d54b4b5a4cf969e81d84b54b47
Aleksandr <soft@vhlv.ru>
2023-02-12 21:40:20 +0300

    feat: create Slide

---
+++ src/components/Slide/Slide.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './_Slide.scss';
+
+
+
+function Slide(props) {
+
+  return (
+    <li className="slide">
+      <figure className="slide__figure">
+        <img
+          src={props.src}
+          alt={props.alt}
+          className="slide__image"
+        />
+        <figcaption className="slide__text">
+          {props.text}
+        </figcaption>
+      </figure>
+    </li>
+
+  );
+}
+
+export default Slide;
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Button from '../Button/Button';
+import Slide from '../Slide/Slide';
 import './_Slider.scss';
 import club27 from '../../images/27club.jpg';
 
 
 function Slider() {
+
   return (
     <>
       <div className="slider">
@@ -14,33 +17,29 @@ function Slider() {
           <li className="slider__dot"></li>
         </ul>
         <ul className="slider__list">
-          <li className="slider__list-item">
-            <figure className="slider__figure">
-              <img
-                src={club27}
-                alt="27 Club"
-                className="slider__image"
-              />
-              <figcaption className="slider__text">
-                Фильм «Почти знаменит»: в тихом омуте черти водятся
-              </figcaption>
-            </figure>
-          </li>
-          <li className="slider__list-item">
-            <figure className="slider__figure">
-              <img
-                src={club27}
-                alt="27 Club"
-                className="slider__image"
-              />
-              <figcaption className="slider__text">
-                Фильм «Почти знаменит»: в тихом омуте черти водятся
-              </figcaption>
-            </figure>
-          </li>
+          <Slide
+            key="1"
+            src={club27}
+            text="Текст слайда 1" />
+          <Slide
+            key="2"
+            src={club27}
+            text="Текст слайда 2" />
         </ul>
-        <button className="slider__button slider__button_left">&larr;</button>
-        <button className="slider__button slider__button_right">&rarr;</button>
+        <Button
+          name="slider-button-left"
+          className="slider__button slider__button_left"
+          aria-label="Slide to left"
+          type="button">
+          &larr;
+        </Button>
+        <Button
+          name="slider-button-right"
+          className="slider__button slider__button_right"
+          aria-label="Slide to right"
+          type="button">
+          &rarr;
+        </Button>
       </div>
     </>
   );
