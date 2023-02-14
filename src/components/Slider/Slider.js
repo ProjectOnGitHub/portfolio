e20ceea8eec93cd59ef9ba8b5a7f36558c975b5a
Aleksandr <soft@vhlv.ru>
2023-02-14 23:30:29 +0300

    feat: add functional for slider button

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Button from '../Button/Button';
 import Slide from '../Slide/Slide';
 import './_Slider.scss';
-import club27 from '../../images/27club.jpg';
 
 
-function Slider() {
+function Slider(props) {
+  let [translate, setTranstlate] = useState(0);
+  let [count, setCount] = useState(0);
+
+  function handleClickSliderButtonLeft() {
+    const width = document.querySelector('.slider').offsetWidth;
+    const slides = document.querySelectorAll('.slide');
+    count <= 0 ? count = slides.length - 1 : --count;
+    setCount(count);
+    translate = count * width;
+    setTranstlate(`${translate}`);
+  }
+
+  function handleClickSliderButtonRight() {
+    const width = document.querySelector('.slider').offsetWidth;
+    const slides = document.querySelectorAll('.slide');
+    count >= slides.length - 1 ? count = 0 : ++count;
+    setCount(count);
+    translate = count * width;
+    setTranstlate(`${translate}`);
+  }
 
   return (
     <>
@@ -16,28 +35,29 @@ function Slider() {
           <li className="slider__dot"></li>
           <li className="slider__dot"></li>
         </ul>
-        <ul className="slider__list">
-          <Slide
-            key="1"
-            src={club27}
-            text="Текст слайда 1" />
-          <Slide
-            key="2"
-            src={club27}
-            text="Текст слайда 2" />
+        <ul className="slider__list" style={{ transform: `translateX(${-translate}px)` }}>
+          {
+            props.projects.map((item) => (
+              <Slide src={item.image} key={item.id}
+                text={item.text} />))
+          }
         </ul>
         <Button
           name="slider-button-left"
           className="slider__button slider__button_left"
           aria-label="Slide to left"
-          type="button">
+          type="button"
+          isClick={handleClickSliderButtonLeft}
+        >
           &larr;
         </Button>
         <Button
           name="slider-button-right"
           className="slider__button slider__button_right"
           aria-label="Slide to right"
-          type="button">
+          type="button"
+          isClick={handleClickSliderButtonRight}
+        >
           &rarr;
         </Button>
       </div>
