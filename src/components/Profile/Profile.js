56c57ddec47ceb9c6bf204e8818b8c3725a8cfb8
Aleksandr <soft@vhlv.ru>
2023-02-17 20:44:25 +0300

    feat: change profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './_Profile.scss';
-import myPhoto from '../../images/my-photo.jpeg';
 
 function Profile() {
   return (
     <>
+
       <div className="profile__info">
         <h1 className='profile__title'>Александр</h1>
         <h2 className='profile__subtitle'>начинающий веб-разработчик</h2>
@@ -16,12 +16,6 @@ function Profile() {
           людей к лучшему.
         </p>
       </article>
-      <figure className="profile__figure">
-        <img
-          src={myPhoto}
-          alt='My Photo'
-          className='profile__photo' />
-      </figure>
     </>
   );
 }
