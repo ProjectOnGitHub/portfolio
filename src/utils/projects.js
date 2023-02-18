f487f0fefc55b0fa61b484388dca1489457e8a45
Aleksandr <soft@vhlv.ru>
2023-02-18 16:04:07 +0300

    feat: add new data for projects

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,35 +1,49 @@
-import club27 from '../images/27club.jpg'
+import club27 from '../images/club27.jpg';
+import shelter from '../images/shelter.jpg';
+import company from '../images/company.jpg';
+import portfolioAlexa from '../images/portfolio-alexa.jpg';
+import kinoprorok from '../images/kinoprorok.jpg';
 
 const projects = [
   {
     id: 1,
-    text: "Проект 1",
-    url: "http://localhost",
+    title: "Сайт на тему клуба 27",
+    description: "Проект на интересную для меня тему. Собственный дизайн. Вёрстка адаптирована под мобильные разрешения.",
+    stack: "React, HTML, SCSS",
+    url: "https://27club.wbdv.ru",
     image: club27,
   },
   {
     id: 2,
-    text: "Проект 2",
-    url: "http://localhost",
-    image: club27,
+    title: "Лендинг компании",
+    description: "Лэндинг для компании по макету из Figma. Свёрстан по различные разрешения.",
+    stack: "HTML, CSS, JavaScript, Webpack",
+    url: "https://company.wbdv.ru",
+    image: company,
   },
   {
     id: 3,
-    text: "Проект 3",
-    url: "http://localhost",
-    image: club27,
+    title: "Лэндинг-портфолио для фотографа",
+    description: "Сайт-портфолио по макету из Figma. С помощью JS реализована возможность менять тему со светлой на тёмную, переключать язык, фильтровать фото по времена года. Вёрстка адаптирована под мобильные разрешения.",
+    stack: "HTML, SCSS, JavaScript",
+    url: "https://portfolio-alexa.wbdv.ru",
+    image: portfolioAlexa,
   },
   {
     id: 4,
-    text: "Проект 4",
-    url: "http://localhost",
-    image: club27,
+    title: "Сайт приюта для животных",
+    description: "Адаптивно свёрстан по макету из Figma. С помощью JS реализована пагинация списка животных и попап, который открывается при клике по фотографии животного.",
+    stack: "React, HTML, SCSS",
+    url: "https://shelter.wbdv.ru",
+    image: shelter,
   },
   {
     id: 5,
-    text: "Проект 5",
-    url: "http://localhost",
-    image: club27,
+    title: "Блог Кино про рок",
+    description: "Собственный дизайн. Свёрстан адаптивно. Натяжка вёрстки на Wordpress с последующей доработка функционала темы",
+    stack: "HTML, SCSS, Webpack, JavaScript, PHP",
+    url: "https://kinoprorok.ru",
+    image: kinoprorok,
   },
 ]
 
