ac6a33b07654be0b0dda6fb362ef485bf09766f4
Aleksandr <soft@vhlv.ru>
2023-02-18 18:24:42 +0300

    feat: add Project to Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './_Portfolio.scss';
-import Slider from '../Slider/Slider';
+import Project from "../Project/Project";
 import projects from '../../utils/projects';
 
 function Portfolio() {
   return (
     <>
       <h2 className="portfolio__title">Portfolio</h2>
-      <Slider
-        projects={projects}
-      />
+      <ul className="portfolio__list">
+        {
+          projects.map((item) => (
+            <Project
+              key={item.id}
+              title={item.title}
+              image={item.image}
+              description={item.description}
+              stack={item.stack}
+              url={item.url}
+            />))
+        }
+      </ul>
     </>
   );
 }
