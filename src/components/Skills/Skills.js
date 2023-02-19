b82ca015b3bbae89e6821d3f0ed6615c2d137fea
Aleksandr <soft@vhlv.ru>
2023-02-19 20:44:55 +0300

    feat: create skills.js

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './_Skills.scss';
-
+import Skill from '../Skill/Skill';
+import skills from '../../utils/skills';
 
 function Skills() {
   return (
     <>
-      <h2 className="projects__title">Skills</h2>
+      <h2 className="skills__title">Skills</h2>
+      <ul className="skills__list">
+        {
+          skills.map((item) => (
+            <Skill
+              name={item.name}
+              key={item.id}
+              title={item.title} />))
+        }
+
+      </ul>
 
     </>
   );
---
+++ src/utils/skills.js
@@ -0,0 +1,40 @@
+const skills = [
+  {
+    id: 1,
+    name: "html",
+    title: "HTML5",
+  },
+  {
+    id: 2,
+    name: "css",
+    title: "CSS3",
+  },
+  {
+    id: 3,
+    name: "scss",
+    title: "SASS (SCSS)",
+  },
+  {
+    id: 4,
+    name: "react",
+    title: "React.js",
+  },
+  {
+    id: 5,
+    name: "webpack",
+    title: "Webpack",
+  },
+  {
+    id: 6,
+    name: "wordpress",
+    title: "WordPress",
+  },
+  {
+    id: 7,
+    name: "git",
+    title: "git",
+  },
+
+]
+
+export default skills;
