88ef2610322e243b4b4fe7f2d5ceffeaabf39461
Aleksandr <soft@vhlv.ru>
2023-02-19 16:41:44 +0300

    feat: create Skills

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './_Skills.scss';
+
+
+function Skills() {
+  return (
+    <>
+      <h2 className="projects__title">Skills</h2>
+
+    </>
+  );
+}
+
+export default Skills;
