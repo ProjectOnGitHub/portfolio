c5050e05cbef5f101bbb21ef1bb0baf5d6362234
Aleksandr <soft@vhlv.ru>
2023-02-20 20:07:10 +0300

    feat: add mobile header menu

--- src/components/HeaderMenu/HeaderMenu.js
+++ src/components/HeaderMenu/HeaderMenu.js
@@ -1,31 +1,58 @@
-import React from 'react';
+import { React, useState } from 'react';
+import Button from '../Button/Button';
 import './_HeaderMenu.scss';
 import { HashLink as Link } from 'react-router-hash-link';
 
 function HeaderMenu(props) {
+
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  function toggleMenu() {
+    isOpenMenu
+      ? setIsOpenMenu(false) || (document.body.classList.remove("scroll-lock"))
+      : setIsOpenMenu(true) || (document.body.classList.add("scroll-lock"));
+
+  }
+
   return (
-    <ul className={`${props.name}__menu`}>
-      <li className={`${props.name}__menu-item`}>
-        <Link to="#about" className={`${props.name}__menu-link`}>
-          About
-        </Link>
-      </li>
-      <li className={`${props.name}__menu-item`}>
-        <Link to="#projects" className={`${props.name}__menu-link`}>
-          Projects
-        </Link>
-      </li>
-      <li className={`${props.name}__menu-item`}>
-        <Link to="#skills" className={`${props.name}__menu-link`}>
-          Skills
-        </Link>
-      </li>
-      <li className={`${props.name}__menu-item`}>
-        <Link to="#contacts" className={`${props.name}__menu-link`}>
-          Contacts
-        </Link>
-      </li>
-    </ul>
+    <>
+      <Button
+        name="header-button"
+        className="header__button"
+        aria-label="Open mobile menu"
+        type="button"
+        isClick={toggleMenu}
+      >
+        <span className={isOpenMenu ? "cross  cross_active" : "cross"}><span className="cross__line"></span></span>
+      </Button>
+      <div
+        className={isOpenMenu ? "overlay overlay_active" : "overlay"}
+        onClick={toggleMenu}>
+      </div>
+      <ul
+        className={isOpenMenu ? `${props.name}__menu ${props.name}__menu_opened` : `${props.name}__menu`}>
+        <li className={`${props.name}__menu-item`} onClick={toggleMenu}>
+          <Link to="#about" className={`${props.name}__menu-link`}>
+            About
+          </Link>
+        </li>
+        <li className={`${props.name}__menu-item`} onClick={toggleMenu}>
+          <Link to="#projects" className={`${props.name}__menu-link`}>
+            Projects
+          </Link>
+        </li>
+        <li className={`${props.name}__menu-item`} onClick={toggleMenu}>
+          <Link to="#skills" className={`${props.name}__menu-link`}>
+            Skills
+          </Link>
+        </li>
+        <li className={`${props.name}__menu-item`} onClick={toggleMenu}>
+          <Link to="#contacts" className={`${props.name}__menu-link`}>
+            Contacts
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 }
 
