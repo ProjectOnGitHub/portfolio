b8b1f4e001fd1025d11ec1d2ff443422c506d681
Aleksandr <soft@vhlv.ru>
2023-02-20 15:31:28 +0300

    feat: create ListItem

---
+++ src/components/ListItem/ListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ListItem(props) {
+  return (
+    <>
+      <li className={props.className}>
+        {props.children}
+      </li>
+    </>
+  );
+}
+
+export default ListItem;
