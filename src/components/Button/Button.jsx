d4d1f37f68eb3569ce6c8070f0ee9dc04bde025e
Aleksandr <soft@vhlv.ru>
2023-02-22 20:55:02 +0300

    refactor: format

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,7 +7,8 @@ function Button(props) {
       className={props.className}
       name={props.name}
       type={props.type}
-      onClick={props.isClick}>
+      onClick={props.isClick}
+    >
       {props.children}
     </button>
   );
