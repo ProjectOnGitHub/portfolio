137da38b7862f01eb021ac50b164fbab0dde73a8
Aleksandr <soft@vhlv.ru>
2023-02-22 10:31:06 +0300

    refactor: replace quote

--- src/utils/contacts.js
+++ src/utils/contacts.js
@@ -1,22 +1,22 @@
 const contacts = [
   {
     id: 1,
-    name: "telegram",
-    title: "@avhlv",
-    url: "https://t.me/avhlv"
+    name: 'telegram',
+    title: '@avhlv',
+    url: 'https://t.me/avhlv',
   },
   {
     id: 2,
-    name: "email",
-    title: "job@vhlv.ru",
-    url: "mailto:job@vhlv.ru"
+    name: 'email',
+    title: 'job@vhlv.ru',
+    url: 'mailto:job@vhlv.ru',
   },
   {
     id: 3,
-    name: "github",
-    title: "ProjectOnGitHub",
-    url: "https://github.com/ProjectOnGitHub"
+    name: 'github',
+    title: 'ProjectOnGitHub',
+    url: 'https://github.com/ProjectOnGitHub',
   },
-]
+];
 
 export default contacts;
--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,23 +1,23 @@
 const menu = [
   {
     id: 1,
-    url: "about",
-    anchor: "About",
+    url: 'about',
+    anchor: 'About',
   },
   {
     id: 2,
-    url: "projects",
-    anchor: "Projects",
+    url: 'projects',
+    anchor: 'Projects',
   },
   {
     id: 3,
-    url: "skills",
-    anchor: "Skills",
+    url: 'skills',
+    anchor: 'Skills',
   },
   {
     id: 4,
-    url: "contacts",
-    anchor: "Contacts",
+    url: 'contacts',
+    anchor: 'Contacts',
   },
-]
+];
 export default menu;
