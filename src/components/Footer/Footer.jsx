7804a065e590cc6e17fd1c4d078f1e979e7e161f
Aleksandr <soft@vhlv.ru>
2023-02-22 10:27:27 +0300

    feat: add Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import Logo from '../Logo/Logo.jsx';
 import './_Footer.scss';
 
 function Footer() {
+  const [year, setYear] = useState();
+
+  function addYear() {
+    const currentYear = new Date().getFullYear();
+    if (currentYear === 2022) {
+      setYear(currentYear);
+    } else setYear(`2022-${+currentYear}`);
+  }
+
+  useEffect(() => {
+    addYear();
+  });
+
   return (
     <footer className="footer">
       <div className="footer__container">
+        <div className="footer__copyright">{`© ${year}`}</div>
         <Logo className="logo logo_small" />
-        <div className="footer__copyright">2023</div>
       </div>
     </footer>
   );
