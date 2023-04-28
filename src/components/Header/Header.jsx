374668c1b85f86af2b9c324f140438b89465e49b
Aleksandr <soft@vhlv.ru>
2023-04-28 14:38:40 +0300

    style: add fixed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
 import './_Header.scss';
 import Logo from '../Logo/Logo.jsx';
 import Nav from '../Nav/Nav.jsx';
@@ -6,8 +6,16 @@ import HeaderMenu from '../HeaderMenu/HeaderMenu.jsx';
 import HeaderSwitcher from '../HeaderSwitcher/HeaderSwitcher.jsx';
 
 function Header() {
+  const [color, setColor] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', () => setColor(window.pageYOffset > 0));
+    }
+  }, []);
+
   return (
-    <header className='header'>
+    <header className={`header ${color ? 'header_fixed' : ''}`}>
       <div className='header__container'>
         <a
           className='header__link'
