9bcabf34b85a8fe5074c42658384d93d1aa552f7
Aleksandr <soft@vhlv.ru>
2023-09-10 22:28:47 +0300

    style: add style for AdminProfile

--- src/components/AdminProfile/AdminProfile.jsx
+++ src/components/AdminProfile/AdminProfile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../Button/Button.jsx';
 import './_AdminProfile.scss';
 
 function AdminProfile(props) {
@@ -6,13 +7,41 @@ function AdminProfile(props) {
     <>
       <h1 className='admin-profile__title'>{props.title}</h1>
       <div className='admin-profile__container'>
-        <div className='admin-profile__info'>
-          <p className='admin-profile__name'>Александр</p>
-          <p className='admin-profile__position'>HTML-верстальщик</p>
-        </div>
+        <p
+          className='admin-profile__name'
+          contentEditable='true'>
+          Александр
+        </p>
+        <span className='admin-profile__description admin-profile__description_name'>
+          Реактировать имя
+        </span>
+        <p
+          className='admin-profile__position'
+          contentEditable='true'>
+          HTML-верстальщик
+        </p>
+        <span className='admin-profile__description admin-profile__description_position'>
+          Реактировать должность
+        </span>
         <div className='admin-profile__text'>
-          <p className='profile__paragraph'>Ушёл из SEO в разработку</p>
+          <p className='admin-profile__paragraph'>
+            <i className='admin-profile__icon'></i>
+            <span
+              className='admin-profile__paragraph-text'
+              contentEditable='true'>
+              Ушёл из SEO в разработку
+            </span>
+            <Button
+              aria-label='Delete button'
+              className='admin-profile__button'
+              name='button-text-delete'
+              type='submit'
+              onClick={props.onClick}></Button>
+          </p>
         </div>
+        <span className='admin-profile__description admin-profile__description_text'>
+          Реактировать текст
+        </span>
       </div>
     </>
   );
