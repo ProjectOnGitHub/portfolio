bc2411c7f96f1d5382eb4a1df4f41e87adef4a28
Aleksandr <soft@vhlv.ru>
2023-09-11 17:43:46 +0300

    feat: add admin section buttons

--- src/components/AdminSection/AdminSection.jsx
+++ src/components/AdminSection/AdminSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './_AdminSection.scss';
+import Button from '../Button/Button.jsx';
 
 function AdminSection(props) {
   return (
@@ -15,6 +16,28 @@ function AdminSection(props) {
         }>
         {props.children}
       </div>
+      <ul className='admin-section__buttons'>
+        <li className='admin-section__buttons-item'>
+          <Button
+            aria-label='Reset button'
+            className='admin-section__button admin-section__button_reset'
+            name='button-reset'
+            type='submit'
+            onClick={props.onClick}>
+            Сбросить изменения
+          </Button>
+        </li>
+        <li className='admin-section__buttons-item'>
+          <Button
+            aria-label='Save button'
+            className='admin-section__button admin-section__button_save'
+            name='button-save'
+            type='submit'
+            onClick={props.onClick}>
+            Сохранить изменения
+          </Button>
+        </li>
+      </ul>
     </section>
   );
 }
