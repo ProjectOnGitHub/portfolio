da47ed052e4ebe734144f2375033854a947eba0e
AV <soft@vhlv.ru>
2023-10-13 19:43:46 +0300

    feat: get skill info

--- src/pages/AdminSkill/AdminSkill.jsx
+++ src/pages/AdminSkill/AdminSkill.jsx
@@ -1,14 +1,23 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getItemInfo } from '../../utils/api';
 import AdminFormImage from '../../components/AdminComponents/AdminFormImage/AdminFormImage.jsx';
 import AdminFormInput from '../../components/AdminComponents/AdminFormInput/AdminFormInput.jsx';
 import AdminSection from '../../components/AdminComponents/AdminSection/AdminSection.jsx';
 
 function AdminSkill() {
+  const { id } = useParams();
+  const [skill, setSkill] = useState({});
+  useEffect(() => {
+    getItemInfo('skills', id).then((newSkill) => setSkill(newSkill));
+  }, [skill]);
+
   return (
     <AdminSection
       className="skill"
       id="skill"
       modifier="skill"
-      title="Редактирование навыка">
+      title={`Редактирование навыка ${skill.title}`}>
       <AdminFormImage>
         <fieldset className="admin-form__fieldset">
           <legend className="admin-form__legend">Редактировать навык</legend>
@@ -17,18 +26,21 @@ function AdminSkill() {
             placeholder="Название навыка"
             required={true}
             type="text"
+            value={skill.title}
           />
           <AdminFormInput
             name="type"
             placeholder="Тип навыка"
             required={true}
             type="text"
+            value={skill.type}
           />
           <AdminFormInput
             name="name"
             placeholder="Название иконки навыка"
             required={true}
             type="text"
+            value={skill.name}
           />
         </fieldset>
       </AdminFormImage>
