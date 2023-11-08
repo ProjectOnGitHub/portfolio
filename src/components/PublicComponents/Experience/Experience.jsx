5a61a8adf6b6405d30ca429c5ca9a5180fa01269
AV <soft@vhlv.ru>
2023-11-08 22:05:44 +0300

    refactor: change request method

--- src/components/PublicComponents/Experience/Experience.jsx
+++ src/components/PublicComponents/Experience/Experience.jsx
@@ -2,31 +2,33 @@ import List from 'components/BaseComponents/List/List';
 import ListItem from 'components/BaseComponents/ListItem/ListItem';
 import ExperienceItem from 'components/PublicComponents/ExperienceItem/ExperienceItem';
 import Section from 'components/PublicComponents/Section/Section';
-
+import useItemInfo from 'hooks/useItemInfo';
 import './_Experience.scss';
 
-function Experience({ experience }) {
-  const reversedExperience = [...experience].reverse();
+function Experience() {
+  const experience = useItemInfo('experience');
+
   return (
     <Section
       className="experience"
       id="experience">
       <h2 className="experience__title">Опыт работы</h2>
       <List className="experience__list">
-        {[...reversedExperience].map((item) => (
-          <ListItem
-            key={item.id}
-            className="experience__list-item">
-            <ExperienceItem
-              className="experience"
-              end={item.end}
-              name={item.name}
-              position={item.position}
-              start={item.start}
-              text={item.text}
-            />
-          </ListItem>
-        ))}
+        {Array.isArray(experience) &&
+          [...experience].reverse().map((item) => (
+            <ListItem
+              key={item.id}
+              className="experience__list-item">
+              <ExperienceItem
+                className="experience"
+                end={item.end}
+                name={item.name}
+                position={item.position}
+                start={item.start}
+                text={item.text}
+              />
+            </ListItem>
+          ))}
       </List>
     </Section>
   );
